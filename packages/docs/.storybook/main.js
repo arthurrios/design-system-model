3644bcefe6ef37a3234a3eea8eb7df105a5269ed
Arthur Rios <arthur.rios007@gmail.com>
2023-07-13 11:34:56 -0300

    second commit

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -8,6 +8,9 @@ module.exports = {
     name: "@storybook/react-vite",
     options: {}
   },
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
   "features": {
     "storyStoreV7": true
   },
