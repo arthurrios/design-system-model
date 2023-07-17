0648f13c3f6ca3d0a742047a54891659da3a3d80
Arthur Rios <arthur.rios007@gmail.com>
2023-07-17 13:17:07 -0300

    add: packages back

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  viteFinal: (config, {
+    configType
+  }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/';
+    }
+    return config;
+  },
+  docs: {
+    autodocs: true
+  }
+};
